c1bff611e7941581dc94627b98a2a6b58fad1f61
Paulina Sadowska <paulinasadowska@Paulinas-MacBook-Pro.local>
2018-12-04 06:29:17 +0100

    upgrade kotlin

--- app/src/test/java/com/nekodev/rocketbrowser/rockets/list/RocketsPresenterTest.kt
+++ app/src/test/java/com/nekodev/rocketbrowser/rockets/list/RocketsPresenterTest.kt
@@ -2,31 +2,38 @@ package com.nekodev.rocketbrowser.rockets.list
 
 import com.nekodev.rocketbrowser.api.Rocket
 import com.nekodev.rocketbrowser.api.RocketService
+import com.nekodev.rocketbrowser.database.VisitDao
 import com.nekodev.rocketbrowser.util.ImmediateSchedulerProvider
 import com.nhaarman.mockito_kotlin.given
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.then
+import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.TestScheduler
 import org.junit.Test
+import java.util.concurrent.TimeUnit
 
 class RocketsPresenterTest {
 
     private val service = mock<RocketService>()
     private val schedulerProvider = ImmediateSchedulerProvider()
     private val view = mock<RocketsContract.View>()
-    private val presenter = RocketsPresenter(service, schedulerProvider)
+    private val dao = mock<VisitDao>()
+    private val presenter = RocketsPresenter(service, schedulerProvider, dao)
 
     @Test
     fun `given rockets fetch success when subscribe then show dialog and fetch launches`() {
         //given
         val rockets = listOf(mock<Rocket>())
         given(service.getRockets()).willReturn(Single.just(rockets))
+        given(dao.getAllVisits()).willReturn(Single.just(emptyList()))
 
         //when
         presenter.subscribe(view)
 
         //then
-        then(view).should().showWelcomeDialog()
+        then(view).should().showWelcomeDialog(0)
         then(view).should().showRockets(rockets)
     }
 
@@ -34,12 +41,32 @@ class RocketsPresenterTest {
     fun `given rockets fetch error when subscribe then show dialog and show error`() {
         //given
         given(service.getRockets()).willReturn(Single.error(Throwable()))
+        given(dao.getAllVisits()).willReturn(Single.just(emptyList()))
 
         //when
         presenter.subscribe(view)
 
         //then
-        then(view).should().showWelcomeDialog()
+        then(view).should().showWelcomeDialog(0)
         then(view).should().showError()
+    }
+
+    @Test
+        fun switchMapTest() {
+        val items = listOf("a", "b", "c", "d", "e", "f")
+        val scheduler = TestScheduler()
+
+        Observable.fromArray(items)
+                .flatMapIterable {it}
+                .switchMap {
+                    Observable.just(it + "x")
+                            .delay(1, TimeUnit.SECONDS, scheduler)
+                }
+                .subscribeBy(
+                    onNext = { println(it)},
+                        onComplete = {println("success")}
+                )
+
+        scheduler.advanceTimeBy(1, TimeUnit.MINUTES)
     }
 }
